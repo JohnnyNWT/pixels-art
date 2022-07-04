7c0586603ca94b1501c1899c863695b0c8084585
JohnnyNWT <jooaaooskt@gmail.com>
2022-07-04 15:57:39 -0300

    adiciona paleta de cores

---
+++ script.js
@@ -0,0 +1,5 @@
+let color = document.getElementsByClassName('color')
+color[0].style.backgroundColor = 'purple'
+color[1].style.backgroundColor = 'blue'
+color[2].style.backgroundColor = 'red'
+color[3].style.backgroundColor = 'green'
