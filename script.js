b11e9e80c438e2dff30593d5aae672458c2d47f1
JohnnyNWT <jooaaooskt@gmail.com>
2022-07-04 17:13:27 -0300

    adiciona grade

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let color = document.getElementsByClassName('color')
-color[0].style.backgroundColor = 'purple'
-color[1].style.backgroundColor = 'blue'
-color[2].style.backgroundColor = 'red'
-color[3].style.backgroundColor = 'green'
+const color = document.getElementsByClassName('color');
+color[0].style.backgroundColor = 'black';
+color[1].style.backgroundColor = 'purple';
+color[2].style.backgroundColor = 'yellow';
+color[3].style.backgroundColor = 'aqua';
