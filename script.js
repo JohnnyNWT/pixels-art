f8c28ac581d2b1fe96bd132d20ef04413aab8fb6
JohnnyNWT <jooaaooskt@gmail.com>
2022-07-05 00:42:50 -0300

    adiciona funçao para  pintar quadrados da grade

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ color[0].style.backgroundColor = 'black';
 color[1].style.backgroundColor = 'purple';
 color[2].style.backgroundColor = 'yellow';
 color[3].style.backgroundColor = 'aqua';
+
+let selecionaCor = document.querySelector('#color-palette')
+let selecionaSquare = document.querySelector('#pixel-board')
+let corSelecionada = 'black'
+
+function selectColor(event) {
+    corSelecionada = event.target.style.backgroundColor
+}
+
+selecionaCor.addEventListener('click', selectColor)
+
+
+function pintar(event) {
+    event.target.style.backgroundColor = corSelecionada
+}
+
+selecionaSquare.addEventListener('click', pintar)
