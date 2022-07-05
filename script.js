b2dcd63b2d75be562867e222b5db2ae90065bfe3
JohnnyNWT <jooaaooskt@gmail.com>
2022-07-05 11:32:45 -0300

    coreção req 7

--- script.js
+++ script.js
@@ -9,14 +9,18 @@ let selecionaSquare = document.querySelector('#pixel-board')
 let corSelecionada = 'black'
 
 function selectColor(event) {
+    for (let index of color) {
+        index.classList.remove('selected')
+    }
+    event.target.classList.add('selected')
     corSelecionada = event.target.style.backgroundColor
+
+    console.log(event.target);
+    console.log(document.querySelectorAll('.color'))
 }
-
 selecionaCor.addEventListener('click', selectColor)
-
 
 function pintar(event) {
     event.target.style.backgroundColor = corSelecionada
 }
-
 selecionaSquare.addEventListener('click', pintar)
